1fe752bcf703540d0ea281bd51891769379f2c31
EC2 Default User <ec2-user@ip-172-31-36-134.us-east-2.compute.internal>
2018-05-13 14:37:23 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,16 +4,30 @@
 
  <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
+  <div class="row">
+       <div class="col-xs-12">
+       <div class="col-sm-offset-2 col-sm-8">
+       <div class="col-lg-offset-3 col-lg-6">
+           
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-       
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        <div class="form-group">
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
+        
+        <div class="form-group">
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        
+            {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
+
+    </div>
+    </div>
+    </div>
+    </div>
 
 @endsection
